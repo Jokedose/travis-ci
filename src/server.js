e1b6bc403e4efee08c87c12f8e84a1ae5381fb9e
jokedose <trirong@gmail.com>
2020-06-17 17:02:26 +0700

    test

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const http = require('http')
 const server = http.createServer((request, response) => {
   console.log(`Received request for URL: ${request.url}`)
   response.writeHead(200)
-  response.end('Hello guys! test')
+  response.end('Hello guys! test 1')
 })
 
 server.listen(8080)
