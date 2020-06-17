d7f87d6ff639b221e63c0b9c3cdde3f756bb161e
jokedose <trirong@gmail.com>
2020-06-17 20:34:12 +0700

    try

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -8,10 +8,10 @@ docker tag asia.gcr.io/${PROJECT_NAME}/${PROJECT_IMAGES}:$TRAVIS_COMMIT asia.gcr
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
-gcloud --quiet config set project $PROJECT_NAME
-gcloud --quiet config set container/cluster $CLUSTER_NAME
-gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+gcloud --quiet config set project ${PROJECT_NAME}
+gcloud --quiet config set container/cluster ${CLUSTER_NAME}
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
 
 gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${PROJECT_IMAGES}
 
@@ -20,4 +20,4 @@ yes | gcloud beta container images add-tag asia.gcr.io/${PROJECT_NAME}/${PROJECT
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/${PROJECT_IMAGES} ${PROJECT_IMAGES}=asia.gcr.io/${PROJECT_NAME}/${PROJECT_IMAGES}:$TRAVIS_COMMIT
+kubectl set image deployment/${PROJECT_IMAGES} image=asia.gcr.io/${PROJECT_NAME}/${PROJECT_IMAGES}:$TRAVIS_COMMIT
