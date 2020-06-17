6b53cf448510640ef6164c20c271460b2fc1cebe
jokedose <trirong@gmail.com>
2020-06-17 23:53:55 +0700

    success

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-'usestrict'
+"usestrict";
 
-consthttp=require('http')
+consthttp = require("http");
 
-constserver=http.createServer((request,response)=>{
-console.log(`ReceivedrequestforURL:${request.url}`)
-response.writeHead(200)
-response.end('Helloguys!testtesttest')
-})
+constserver = http.createServer((request, response) => {
+  console.log(`ReceivedrequestforURL:${request.url}`);
+  response.writeHead(200);
+  response.end("Hello guys!test test test");
+});
 
-server.listen(8080)
+server.listen(8080);
